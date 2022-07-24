855f43c531b87975a21aaecff6a66a4fd707a27c
Milton <web3@daffodilvarsity.edu.bd>
2022-07-24 11:21:24 +0600

    Initial Project

---
+++ routes/web.php
@@ -0,0 +1,82 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\RolesController;
+use App\Http\Controllers\Admin\PermissionsController;
+use App\Http\Controllers\Admin\UsersController;
+use App\Http\Controllers\Admin\AuditLogsController;
+use App\Http\Controllers\Auth\ChangePasswordController;
+use App\Http\Controllers\ProfileController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::redirect('/', '/login');
+Route::get('/home', function () {
+    if (session('status')) {
+        return redirect()->route('admin.home')->with('status', session('status'));
+    }
+    return redirect()->route('admin.home');
+});
+
+
+
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['auth']], function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    // Permissions
+    Route::delete('permissions/destroy', [PermissionsController::class,'massDestroy'])->name('permissions.massDestroy');
+    Route::resource('permissions', PermissionsController::class);
+
+    // Roles
+    Route::delete('roles/destroy', [RolesController::class, 'massDestroy'])->name('roles.massDestroy');
+    Route::resource('roles', RolesController::class);
+
+    // Users
+    Route::delete('users/destroy', [UsersController::class,'massDestroy'])->name('users.massDestroy');
+    Route::resource('users', UsersController::class);
+
+    // Audit Logs
+    Route::resource('audit-logs', AuditLogsController::class, ['except' => ['create', 'store', 'edit', 'update', 'destroy']]);
+});
+
+
+Route::group(['prefix' => 'profile', 'as' => 'profile.', 'middleware' => ['auth']], function () {
+    // Change password
+    if (file_exists(app_path('Http/Controllers/Auth/ChangePasswordController.php'))) {
+        Route::get('password', [ChangePasswordController::class,'edit'])->name('password.edit');
+        Route::post('password', [ChangePasswordController::class,'update'])->name('password.update');
+        Route::post('profile', [ChangePasswordController::class,'updateProfile'])->name('password.updateProfile');
+        Route::post('profile/destroy', [ChangePasswordController::class,'destroy'])->name('password.destroyProfile');
+
+
+        Route::get('/edit',[ProfileController::class,'edit'])->name('edit');
+        Route::post('/edit',[ProfileController::class,'updateProfile'])->name('updateProfile');
+
+
+    }
+
+});
+
+
+
+
+
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
